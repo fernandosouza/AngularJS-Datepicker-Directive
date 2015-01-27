7838a7175f1ac89a30a1fb693dbc5811eb2073ab
Fernando Souza <nandosouzafilho@gmail.com>
2015-01-27 18:41:05 -0300

    Module config

--- module.js
+++ module.js
@@ -1,4 +1,15 @@
 (function(){
-	angular
-		.module('angular-datepicker', []);
+    angular
+        .module('angular-datepicker', [])
+        .config(function(){
+            $.fn.datepicker.dates['pt'] = {
+                days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"],
+                daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"],
+                daysMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"],
+                months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+                monthsShort: ["Jan", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                today: "Hoje",
+                clear: "Apagar"
+            };
+        });
 })();
