acc461025d856af5493fcccbb6f36364346a0055
Fernando Souza <nandosouzafilho@gmail.com>
2015-01-27 20:37:43 -0300

    onChangeDate demo

--- demo/app.js
+++ demo/app.js
@@ -8,7 +8,10 @@
 		.module('app')
 		.controller('PickDate', PickDate);
 
-	function PickDate(){
-		
+	function PickDate($scope){
+		$scope.date = '2009-06-25T00:00:00-03:00';
+        $scope.onChangeDateFn = function(){
+            console.log(arguments);
+        }
 	}
 })();
